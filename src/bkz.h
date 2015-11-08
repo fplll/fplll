04295325724af5ecdb0be431ce50d6a995747324
Martin R. Albrecht <martinralbrecht@googlemail.com>
2015-11-08 00:13:14 +0000

    fix enableLinearPruning (again!)

--- src/bkz.h
+++ src/bkz.h
@@ -98,8 +98,8 @@ public:
     pruning.resize(blockSize);
     for(int k=0; k< startDescent; k++)
       pruning[k] = 1.0;
-    for(int k=0; k<blockSize; k++)
-      pruning[k] = ((double)(blockSize-k-1))/blockSize;
+    for(int k=0; k<blockSize-startDescent; k++)
+      pruning[startDescent+k] = ((double)(blockSize-k-1))/blockSize;
   }
                      
 };
