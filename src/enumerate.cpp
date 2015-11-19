e95448685509c8c090a5a418b453c8c5176c4603
Michael Walter <miwalter@eng.ucsd.edu>
2015-11-19 11:33:18 -0800

    bugfix

--- src/enumerate.cpp
+++ src/enumerate.cpp
@@ -113,7 +113,6 @@ bool Enumeration::enumerateLoop(enumf& newMaxDist, int& newKMax) {
       }
 
       for (int j = centerLoopBg[k]; j > k; j--) {
-        //~ centerPartSums[k][j] = centerPartSums[k][j + 1] - x[j] * mut[k][j];
         centerPartSums[k][j] = centerPartSums[k][j + 1] + center_summand(k, j);
       }
       enumf newCenter = centerPartSums[k][k + 1];
@@ -227,8 +226,13 @@ void Enumeration::enumerate(MatGSO<Integer, FT>& gso, FT& fMaxDist, long maxDist
   prepareEnumeration(maxDist, subTree, solvingSVP);
   enumerate(maxDist, normExp, evaluator, pruning);
   
-  fMaxDistNorm = dual ? enumf(1.0)/maxDist : maxDist; // Exact
-  fMaxDist.mul_2si(fMaxDistNorm, normExp - maxDistExpo);
+  fMaxDistNorm = maxDist; // Exact
+  
+  if (dual) {
+    fMaxDistNorm.mul_2si(fMaxDist, maxDistExpo - normExp);
+  } else {
+    fMaxDist.mul_2si(fMaxDistNorm, normExp - maxDistExpo);
+  }
   
   if (dual && !evaluator.solCoord.empty()) reverseBySwap(evaluator.solCoord, 0, d-1);
 }
