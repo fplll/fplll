54b9a71ffbcb01980b5f64bff8483eb1aaee0f92
Tancrède Lepoint <tancrede.lepoint@sri.com>
2016-10-24 13:15:10 -0400

    correcting the travis check style with macOS

--- .travis-check-style.sh
+++ .travis-check-style.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Add LLVM toolchain
-sudo add-apt-repository 'deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-3.9 main'
-wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
-
-# Install clang-format
-sudo apt-get update -qq 
-sudo apt-get install -qq -y clang-format-3.9
-
-# If clang-format modifies files, the code does not adhere to the project standards
-export CLANGFORMAT=clang-format-3.9
 make check-style
 if [[ $(git status -s) ]]; 
 then 
