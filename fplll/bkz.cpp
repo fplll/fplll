208cb782e79fbedd8f5d7a4b0ab74203bd3eabf4
Michael Walter <michael.walter@swel.com>
2017-07-13 15:12:38 +0200

    remove bounded_lll optimizations (potentially dangerous, needs more testing first)

--- fplll/bkz.cpp
+++ fplll/bkz.cpp
@@ -105,12 +105,9 @@ bool BKZReduction<ZT, FT>::svp_preprocessing(int kappa, int block_size, const BK
   FPLLL_DEBUG_CHECK(param.strategies.size() > block_size);
 
   int lll_start = (param.flags & BKZ_BOUNDED_LLL) ? kappa : 0;
-  if (!lll_obj.lll(lll_start, lll_start, kappa + block_size, lll_start))
+  if (!lll_obj.lll(lll_start, lll_start, kappa + block_size, 0))
   {
-    if (!lll_obj.lll(lll_start, lll_start, kappa + block_size, 0))
-    {
-      throw std::runtime_error(RED_STATUS_STR[lll_obj.status]);
-    }
+    throw std::runtime_error(RED_STATUS_STR[lll_obj.status]);
   }
   if (lll_obj.n_swaps > 0)
     clean = false;
@@ -119,7 +116,7 @@ bool BKZReduction<ZT, FT>::svp_preprocessing(int kappa, int block_size, const BK
   for (auto it = preproc.begin(); it != preproc.end(); ++it)
   {
     int dummy_kappa_max = num_rows;
-    BKZParam prepar = BKZParam(*it, param.strategies, LLL_DEF_DELTA, BKZ_GH_BND | BKZ_BOUNDED_LLL);
+    BKZParam prepar = BKZParam(*it, param.strategies, LLL_DEF_DELTA, BKZ_GH_BND);
     clean &= tour(0, dummy_kappa_max, prepar, kappa, kappa + block_size);
   }
 
