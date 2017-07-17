d07770ec5e348a756ac858b16c018586e5f72428
Michael Walter <michael.walter@swel.com>
2017-07-17 14:48:59 +0200

    make check-style happy

--- fplll/enum/enumerate.cpp
+++ fplll/enum/enumerate.cpp
@@ -95,12 +95,12 @@ void EnumerationDyn<ZT, FT>::enumerate(int first, int last, FT &fmaxdist, long f
     fr      = _gso.get_r_exp(i + first, i + first, rexpo);
     normexp = max(normexp, rexpo + fr.exponent());
   }
-  // normalization is multiplication by 2^(-normexp). in case of dual, we are normalizing 
+  // normalization is multiplication by 2^(-normexp). in case of dual, we are normalizing
   // the inverse of r, so we negate the normalizing exponent
   if (dual)
   {
     normexp *= -1;
-  }  
+  }
   fmaxdistnorm.mul_2si(fmaxdist, fmaxdistexpo - normexp);
   maxdist = fmaxdistnorm.get_d(GMP_RNDU);
   _evaluator.set_normexp(normexp);
