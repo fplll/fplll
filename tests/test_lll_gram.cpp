0dbe87c3b0eea1fc81574819310785843117332e
Laurent Grémy <laurent.gremy@ens-lyon.fr>
2017-09-13 11:07:27 +0200

    Checkstyle happy.

--- tests/test_lll_gram.cpp
+++ tests/test_lll_gram.cpp
@@ -13,13 +13,13 @@
    You should have received a copy of the GNU Lesser General Public License
    along with fplll. If not, see <http://www.gnu.org/licenses/>. */
 
-#include <test_utils.h>
 #include <cstring>
 #include <fplll.h>
 #include <gso.h>
 #include <gso_gram.h>
 #include <gso_interface.h>
 #include <lll.h>
+#include <test_utils.h>
 
 using namespace std;
 using namespace fplll;
