91c9af14d99247155369b604ca39b01f3581b658
arnsip <as447@uowmail.edu.au>
2018-01-20 00:48:38 +1100

    re-fresh encore

--- tests/test_hnf.cpp
+++ tests/test_hnf.cpp
@@ -17,7 +17,7 @@
 #include <../fplll/hnf.cpp>
 
 #include <cstring>
-#include <fplll.h>
+// #include <fplll.h>
 #include <test_utils.h>
 
 using namespace std;
@@ -26,68 +26,6 @@ using namespace fplll;
 #ifndef TESTDATADIR
 #define TESTDATADIR ".."
 #endif
-
-/**
-   @brief Test the tester.
-
-   @param A
-   @return zero on success.
-*/
-
-int test_test(ZZ_mat<mpz_t> &A)
-{
-  // returns 0 if A is a HNF, since A is given as non-HNF it should return 1
-  int is_reduced = is_hnf_reduced(A);
-
-  if (!is_reduced)
-    {cerr << "is_hnf_reduced reports success when it should not" << endl;}
-
-  return is_reduced;
-}
-
-/**
-   @brief Test HNF reduction.
-
-   @param A                test matrix
-   @param method           HNF method to test
-
-   @return zero on success.
-*/
-
-int test_hnf(ZZ_mat<mpz_t> &A, HNFMethod method)
-{
-
-  ZZ_mat<mpz_t> tmp = A;
-
-  int status = 0;
-
-  // zero on success
-  if (test_test(A))
-  {
-    return 1;
-  }
-
-  // zero on success
-  status = hnf(A, method);
-
-  // a bit obsolete since there's no error case (yet), but we leave it for the future
-  if (status != RED_SUCCESS)
-  {
-    cerr << "HNF reduction failed with error '" << get_red_status_str(status) << endl;
-    return status;
-  }
-
-  // zero on success
-  status = in_hnf(A, tmp);
-
-  if (status)
-  {
-    cerr << "Output of HNF reduction is not HNF reduced with method " << HNF_METHOD_STR[method]
-         << " test with matrix returned " << status << endl;
-  }
-
-  return status;
-}
 
 /**
    @brief Test HNF for matrix stored in file pointed to by `input_filename`.
@@ -100,33 +38,37 @@ int test_hnf(ZZ_mat<mpz_t> &A, HNFMethod method)
 
 int test_filename(const char *input_filename, HNFMethod method)
 {
-  ZZ_mat<mpz_t> A;
+  ZZ_mat<mpz_t> A,tmp;
+
+  // have the status
   int status = 0;
+
+  // read the matrix
   status |= read_matrix(A, input_filename);
   if (status) {cerr << "problem when reading matrix\n";}
-  cerr << "original matrix :\n" << A << endl;
-  status |= test_hnf(A, method);
-  if (status) {cerr << "HNF badly reduced\n";}
+  tmp = A;
+  cerr << "original matrix :\n" << tmp << endl;
+
+  // zero on success
+  if (!is_hnf_reduced(A))
+  {
+    cerr << "is_hnf_reduced reports success when it should not" << endl;
+    return 1;
+  }
+
+  // test if reduction was correct : zero on success
+  status = in_hnf(A, tmp);
+
   cerr << "final matrix :\n" << A << endl;
+
+  if (status != RED_SUCCESS)
+  {
+    cerr << "Output of HNF reduction is not HNF reduced with method " << HNF_METHOD_STR[method]
+         // << " : test with matrix returned " << get_red_status_str(status) << endl;
+         << " : test with matrix returned " << status << endl;
+  }
+
   return status;
-}
-
-/**
-   @brief Construct d × (d+1) integer relations matrix with bit size b and test HNF.
-
-   @param d                dimension
-   @param b                bit size
-   @param method           HNF method to test
-
-   @return zero on success
-*/
-
-int test_int_rel(int d, int b, HNFMethod method)
-{
-  ZZ_mat<mpz_t> A;
-  A.resize(d, d + 1);
-  A.gen_intrel(b);
-  return test_hnf(A, method);
 }
 
 int main(int /*argc*/, char ** /*argv*/)
@@ -136,14 +78,6 @@ int main(int /*argc*/, char ** /*argv*/)
   // status |= test_filename(TESTDATADIR "/tests/lattices/dim55_in", HM_CLASSIC);
   status |= test_filename(TESTDATADIR "/tests/lattices/dim55_in", HM_XGCD);
   // status |= test_filename(TESTDATADIR "/tests/lattices/dim55_in", HM_PERNETSTEIN);
-
-  // status |= test_int_rel(50, 1000, HM_CLASSIC);
-  // status |= test_int_rel(50, 1000, HM_XGCD);
-  // status |= test_int_rel(50, 1000, HM_PERNETSTEIN);
-
-  // status |= test_int_rel(30, 2000, HM_CLASSIC);
-  // status |= test_int_rel(30, 2000, HM_XGCD);
-  // status |= test_int_rel(30, 2000, HM_PERNETSTEIN);
 
   // status |= test_filename(TESTDATADIR "/tests/lattices/example_out", HM_CLASSIC);
   status |= test_filename(TESTDATADIR "/tests/lattices/example_out", HM_XGCD);
