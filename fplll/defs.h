8c52c959bc4f8ed4a5caf8886759f308fd779ffc
Isuru Fernando <isuruf@gmail.com>
2019-05-15 11:10:27 -0500

    Fix formatting of code

--- fplll/defs.h
+++ fplll/defs.h
@@ -31,29 +31,29 @@
 
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
-#include <winsock2.h>
 #include <stdint.h>
+#include <winsock2.h>
 
 // Taken from https://stackoverflow.com/a/26085827
-static inline int gettimeofday(struct timeval * tp, struct timezone * tzp)
+static inline int gettimeofday(struct timeval *tp, struct timezone *tzp)
 {
-    // Note: some broken versions only have 8 trailing zero's, the correct epoch has 9 trailing zero's
-    // This magic number is the number of 100 nanosecond intervals since January 1, 1601 (UTC)
-    // until 00:00:00 January 1, 1970
-    static const uint64_t EPOCH = ((uint64_t) 116444736000000000ULL);
+  // Note: some broken versions only have 8 trailing zero's, the correct epoch has 9 trailing zero's
+  // This magic number is the number of 100 nanosecond intervals since January 1, 1601 (UTC)
+  // until 00:00:00 January 1, 1970
+  static const uint64_t EPOCH = ((uint64_t)116444736000000000ULL);
 
-    SYSTEMTIME  system_time;
-    FILETIME    file_time;
-    uint64_t    time;
+  SYSTEMTIME system_time;
+  FILETIME file_time;
+  uint64_t time;
 
-    GetSystemTime( &system_time );
-    SystemTimeToFileTime( &system_time, &file_time );
-    time =  ((uint64_t)file_time.dwLowDateTime )      ;
-    time += ((uint64_t)file_time.dwHighDateTime) << 32;
+  GetSystemTime(&system_time);
+  SystemTimeToFileTime(&system_time, &file_time);
+  time = ((uint64_t)file_time.dwLowDateTime);
+  time += ((uint64_t)file_time.dwHighDateTime) << 32;
 
-    tp->tv_sec  = (long) ((time - EPOCH) / 10000000L);
-    tp->tv_usec = (long) (system_time.wMilliseconds * 1000);
-    return 0;
+  tp->tv_sec  = (long)((time - EPOCH) / 10000000L);
+  tp->tv_usec = (long)(system_time.wMilliseconds * 1000);
+  return 0;
 }
 #endif
 
