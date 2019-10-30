e374adf7609c47f3b44bd49234c0c2599bfef2aa
Marios <xlopmar@gmail.com>
2019-10-30 18:03:39 +0200

    Basic BKZ done, all different flags provided, last three must be tested against the API.

--- tutorials/bkz_reduction_tutorial.cpp
+++ tutorials/bkz_reduction_tutorial.cpp
@@ -21,11 +21,32 @@ using namespace fplll;
  * g++ -std=c++11 -march=native -o3 lll_reduction_tutorial.cpp -lfplll -mpfr -lgmp -o <executable_name_here>
  */
 
-void strategize(vector<Strategies> &strategy, block_size)
+/** 
+ * NOTE: We are using BKZ_DEFAULT as the BKZ method. Additional flags (and a combination thereof, with |)
+ * that can be used as well are:
+ * BKZ_VERBOSE: print additional information during reduction
+ * BKZ_NO_LLL: do not run LLL before block reduction (use at your own risk)
+ * BKZ_MAX_LOOPS: terminate after max_loops iterations
+ * BKZ_MAX_TIME: terminate after max_time time
+ * BKZ_BOUNDED_LLL: only run LLL in current block during SVP preprocessing (use at your own risk)
+ * BKZ_AUTO_ABORT: heuristically terminate the reduction if progress stalls
+ * BKZ_DUMP_GSO: after every iteration write the shape of the current basis to a file
+ * BKZ_GH_BND: use the Gaussian heuristic to reduce the enumeration bound of possible
+ * BKZ_SD_VARIANT: run SD-BKZ
+ * BKZ_SLD_RED: run slide reduction 
+ */
+
+
+void strategize(int block_size, vector<Strategy> &strategies)
 {
 	for (int i = 0; i <= block_size; i++) 
 	{
-		
+		Strategy strategy = Strategy::EmptyStrategy(i);
+		if ((i == 10) || (i == 20) || (i == 30)) 
+		{
+			strategy.preprocessing_block_sizes.emplace_back(i / 2);
+		}
+		strategies.emplace_back(move(strategy));
 	}
 }
 
@@ -40,7 +61,7 @@ void bkz_reduction_default (ZZ_mat<mpz_t> &base)
 	int block_size = round(sqrt(base.get_cols())), precision = 0; /* Block size is set as described above */
 	/**
 	 * This function implements the BKZ reduction. BKZ_DEFAULT indicates that the default method will be
-	 * used (no pruning vector). FT_DOUBLE sets the real number type to double, necessary for Gram-Schmidt step.
+	 * used. FT_DOUBLE sets the real number type to double, necessary for Gram-Schmidt step.
 	 * Other options are FP_MPFR and FT_DEFAULT. In the case of FT_MPFR, the precision (now set to 0)
 	 * MUST be other than 0 (128 is recommended, as it is used on test files).
 	 */
@@ -60,20 +81,28 @@ void bkz_reduction_with_parameters (ZZ_mat<mpz_t> &base)
 	 * preprocessing block_sizes. This will be our pruning vector. For the purposes of this
 	 * tutorial, the same strategy as the one used in test_bkz.cpp will be used here.
 	 */
-	vector<Strategies> strategy;
-	strategize(strategy, block_size);
-	status = bkz_reduction(base)
+	vector<Strategy> strategies;
+	strategize(block_size, strategies);
+	/* This class generates the BKZ parameters */
+	BKZParam parameters (block_size, strategies);
+	/**
+	 * The flag for the parameter is set to BKZ_DEFAULT. 
+	 */
+	parameters.flags = BKZ_DEFAULT; 
+	status = bkz_reduction(&base, NULL, parameters, FT_DEFAULT, 53);
+	cout << base << endl;
 }
 
 
  int main (int argc, char *argv[])
  {
  	ZZ_mat<mpz_t> base;
- 	base.resize(5, 5);
- 	base.gen_uniform(4);
+ 	base.resize(30, 30);
+ 	base.gen_uniform(10);
  	cout << base << endl;
  	cout << endl;
  	bkz_reduction_default (base);
+ 	bkz_reduction_with_parameters(base);
  	cout << endl;
  	return 0;
  }
