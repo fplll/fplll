8b12b39bfb637316f08e00b24dc7bc9543da873e
Joe Rowell <joe.rowell.2015@live.rhul.ac.uk>
2019-10-30 09:07:45 +0000

    Added a few bits of documentation to enumerate_ext.h

--- fplll/enum/enumerate_ext.h
+++ fplll/enum/enumerate_ext.h
@@ -31,22 +31,34 @@ FPLLL_BEGIN_NAMESPACE
 /**
  * Callback function given to external enumeration library.
  *
- * You have to pass a pointer to an array 'enumf mu[mudim][mudim]'.
- * When flag mutranspose is true then mutransposed is actually stored there.
- * You have to pass a pointer to an array 'enumf rdiag[mudim]'
- * and an array 'enumf pruning[mudim]'.
- * Note: for dual SVP you also get mu and rdiag as is,
- * so the external library must make the respective changes to Mu and Rdiag itself.
+ * @param[out] mu - this is a pointer to an array 'enumf mu[mudim][mudim]'. 
+ * 	       Upon return, this array will contain
+ * 	       the mu values for the lattice basis.
+ * 	       Note that the array pointed to by this argument needs to be contiguous, 
+ * 	       otherwise there will be write errors.
+ * 	       This means that the only acceptable arguments are pointers to 
+ * 	       variable-length arrays, 1D std::vector of dimension mudim*mudim, or 
+ * 	       1D raw(resp. std::array) arrays of dimension mudim*mudim.
+ * @param[in]  mudim - the number of rows(resp. columns) in the mu array. 
+ * @param[out] mutranspose - when true, mutranspose is stored in the mu param. 
+ *                           Otherwise, mu is stored in the mu param. 
+ * 			Storing mutranspose allows for more efficient memory access, as accesses will be contiguous. 
+ * @param[out] rdiag - a pointer to an array 'enumf rdiag[mudim]'. 
+ * 	               Upon return, this will contain the squared norms of the Gram-Schmidt vectors. 
+ * @param[out] pruning - a pointer to an array 'enumf pruning[mudim]'. Upon return, this will contain the pruning coefficients 
+ * 	                 for enumeration. In rigorous enumeration, this array will consist solely of 1's.
  */
-typedef void(extenum_cb_set_config)(enumf *mu, size_t mudim, bool mutranspose, enumf *rdiag,
-                                    enumf *pruning);
+typedef void(extenum_cb_set_config)(enumf*  mu, size_t mudim, bool mutranspose, enumf* rdiag,
+                                    enumf*  pruning);
 
 /**
- * Callback function given to external enumeration library.
- *
- * Pass a new solution and its length to Evaluator, it returns the new enumeration bound.
+ * Callback function given to external enumeration library. 
+ * Passes a new solution and its length to Evaluator, returning the new enumeration bound.
+ * @param dist - the norm of the new solution.
+ * @param sol - a pointer to the new solution. 
+ * @return The new enumeration bound.
  */
-typedef enumf(extenum_cb_process_sol)(enumf dist, enumf *sol);
+typedef enumf(extenum_cb_process_sol)(enumf dist, enumf*  sol);
 
 /**
  * Callback function given to external enumeration library.
@@ -76,9 +88,10 @@ typedef uint64_t(extenum_fc_enumerate)(int dim, enumf maxdist,
                                        bool dual /*=false*/, bool findsubsols /*=false*/
 );
 
-// set & get external enumerator (nullptr => disabled)
+// set & get external enumerator. If extenum = nullptr =>, then this interface is disabled.
 void set_external_enumerator(std::function<extenum_fc_enumerate> extenum = nullptr);
 std::function<extenum_fc_enumerate> get_external_enumerator();
+
 
 template <typename ZT, typename FT> class ExternalEnumeration
 {
@@ -88,6 +101,7 @@ public:
   {
   }
 
+  
   bool enumerate(int first, int last, FT &fmaxdist, long fmaxdistexpo,
                  const vector<enumf> &pruning = vector<enumf>(), bool dual = false);
 
@@ -104,8 +118,8 @@ private:
   Evaluator<FT> &_evaluator;
   vector<enumf> _pruning;
   long _normexp;
-  uint64_t _nodes;
 
+  uint64_t _nodes;
   bool _dual;
   int _d, _first;
   enumf _maxdist;
