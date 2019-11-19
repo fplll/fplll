35cbda48302669b4ae2ee5fa87c17cf5676c8f20
Martin R. Albrecht <martinralbrecht@googlemail.com>
2019-11-19 21:05:06 +0000

    Revert "Some basic sanity checking in MatGSO"
    This reverts commit f9436a3681f3118cd6476d88320ca11dc1b7e5f3.

--- fplll/gso_interface.h
+++ fplll/gso_interface.h
@@ -705,14 +705,7 @@ template <class ZT, class FT> inline FT &MatGSOInterface<ZT, FT>::get_r(FT &f, i
 
 template <class ZT, class FT> inline bool MatGSOInterface<ZT, FT>::update_gso_row(int i)
 {
-  bool r = update_gso_row(i, i);
-#ifdef DEBUG
-  // if a user submits a non-gram Matrix as a Gram matrix, this might happen
-  FT tmp;
-  get_r(tmp, i, i);
-  FPLLL_DEBUG_CHECK(tmp >= 0);
-#endif
-  return r;
+  return update_gso_row(i, i);
 }
 
 template <class ZT, class FT> inline void MatGSOInterface<ZT, FT>::set_r(int i, int j, FT &f)
