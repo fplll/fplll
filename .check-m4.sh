d9fc104423adc7b615af4094b7f7146edc05d89c
cr-marcstevens <github@marc-stevens.nl>
2019-11-25 09:48:11 +0100

    Check M4 version with make check-style

---
+++ .check-m4.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+for f in m4/*.m4 ; do
+	oldserial=$(grep "^#serial" $f | grep -o "[0-9]*" | head -n1)
+	if [ -z $oldserial ]; then
+		continue
+	fi
+
+	echo "Checking for newer version of: $f"
+	if wget -t 1 -T 0.5 -O $f.tmp "https://raw.githubusercontent.com/autoconf-archive/autoconf-archive/master/$f" >/dev/null 2>/dev/null ; then
+		newserial=$(grep "^#serial" $f.tmp | grep -o "[0-9]*" | head -n1)
+	else
+		echo "WGET error"
+		continue
+	fi
+	if [ -z $newserial ]; then
+		echo "Error checking newer version"
+		continue
+	fi
+
+	if [ $oldserial -eq $newserial ]; then
+		echo "Current version is latest version"
+	else
+		echo "*** UPDATE AVAILABLE for $f: version $oldserial (current) => $newserial (latest) ***"
+	fi
+done
