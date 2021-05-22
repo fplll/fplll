b12469705aeeeaf1b90fa17209c6704927040b1a
cr-marcstevens <github@marc-stevens.nl>
2021-05-22 15:08:57 +0200

    Do bounds checking on vector matrix product

--- fplll/util.h
+++ fplll/util.h
@@ -41,6 +41,8 @@ template <class ZT>
 void vector_matrix_product(vector<ZT> &result, const vector<ZT> &x, const Matrix<ZT> &m)
 {
   int nrows = m.get_rows(), ncols = m.get_cols();
+  if (x.size() != nrows)
+   throw std::runtime_error("vector_matrix_product(): dimensions do not match!");
   gen_zero_vect(result, ncols);
   for (int i = 0; i < nrows; i++)
     for (int j = 0; j < ncols; j++)
@@ -53,6 +55,8 @@ template <class ZT>
 void vector_matrix_product(NumVect<ZT> &result, const NumVect<ZT> &x, const Matrix<ZT> &m)
 {
   int nrows = m.get_rows(), ncols = m.get_cols();
+  if (x.size() != nrows)
+   throw std::runtime_error("vector_matrix_product(): dimensions do not match!");
   result.gen_zero(ncols);
   for (int i = 0; i < nrows; i++)
     for (int j = 0; j < ncols; j++)
